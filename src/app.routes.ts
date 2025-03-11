05a170c3f9d9e5aa443ac07fd582d73fd9c29b8a
Irvan Malik <irvanma@gnuweeb.org>
2025-03-11 12:54:38 +0700

    feat(app.route): add routes
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,7 +1,12 @@
 import Elysia from "elysia";
 
 import { RootController } from "./routes/static/infra/static.controller";
+import { GetController } from "./routes/get/infra/get.controller";
+import { PostController } from "./routes/post/infra/post.controller";
 
-const routes = new Elysia({ prefix: "v1" }).use(RootController);
+const routes = new Elysia({ prefix: "v2" })
+  .use(RootController)
+  .use(GetController)
+  .use(PostController);
 
 export { routes as AppRoutes };
