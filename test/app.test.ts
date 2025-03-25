84945fba51e185259dcf1e2f3d9b377bc5e51c3f
Irvan Malik <irvanma@gnuweeb.org>
2025-03-26 01:37:08 +0700

    feat(test): add passworded test
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "bun:test";
 import { app } from "../src/app";
 import { valkey } from "../src/shared/infrastructure/valkey";
+import { config } from "../config";
 
 describe("RB-BE", () => {
   it("[GET /v2] Return a response", async () => {
@@ -12,10 +13,9 @@ describe("RB-BE", () => {
       JSON.stringify({
         title: "Realm Bin BE",
         description: "The backend for Realm Bin. Made in ElysiaJS.",
-        version: "1.0.0",
+        version: "1.0.1",
         apiPrefix: "v2",
-        environment:
-          Bun.env.RUNNING_ENV === "production" ? "production" : "development",
+        environment: config.environment,
       })
     );
   });
@@ -24,7 +24,7 @@ describe("RB-BE", () => {
       .handle(
         new Request("http://localhost:9944/v2/post", {
           method: "POST",
-          body: JSON.stringify({ content: "Hello, World!" }),
+          body: JSON.stringify({ content: "Hello, World!", isLocked: "false" }),
           headers: {
             "Content-Type": "application/json",
           },
@@ -36,6 +36,7 @@ describe("RB-BE", () => {
 
     expect(res.id).toEqual(expect.any(String));
     expect(res.paste).toEqual("Hello, World!");
+    expect(res.isLocked).toEqual("false");
 
     describe("RB-BE (Inheriting)", () => {
       it("[GET /v2/get/:id] Get a paste", async () => {
@@ -44,6 +45,55 @@ describe("RB-BE", () => {
             new Request(`http://localhost:9944/v2/get/${res.id}`, {
               method: "GET",
             })
+          )
+          .then((data) => data.text());
+
+        // cleanup
+        valkey.del(res.id);
+
+        expect(response).toBe(
+          JSON.stringify({
+            id: res.id,
+            paste: res.paste,
+          })
+        );
+      });
+    });
+  });
+  it("[POST /v2/post] Send a passworded paste", async () => {
+    const response = await app
+      .handle(
+        new Request("http://localhost:9944/v2/post", {
+          method: "POST",
+          body: JSON.stringify({
+            content: "Hello, World!",
+            isLocked: "true",
+            password: "password",
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+      )
+      .then((res) => res.text());
+
+    const res = JSON.parse(response);
+
+    expect(res.id).toEqual(expect.any(String));
+    expect(res.paste).toEqual("Hello, World!");
+    expect(res.isLocked).toEqual("true");
+    expect(res.password).toEqual(expect.any(String));
+
+    describe("RB-BE (Inheriting)", () => {
+      it("[GET /v2/get/:id] Get a paste", async () => {
+        const response = await app
+          .handle(
+            new Request(
+              `http://localhost:9944/v2/get/${res.id}?password=password`,
+              {
+                method: "GET",
+              }
+            )
           )
           .then((data) => data.text());
 
