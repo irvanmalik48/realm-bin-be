cadc4a03a8d959f9a257fc1bbe01662f85355e21
Irvan Malik <irvanma@gnuweeb.org>
2025-03-27 00:11:42 +0700

    fix(shared/infra/disable-swagger): fix derp
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/shared/infrastructure/disable-swagger.ts
+++ src/shared/infrastructure/disable-swagger.ts
@@ -2,7 +2,7 @@ import Elysia from "elysia";
 
 export const DisableSwaggerController = new Elysia({
   name: "Disable Swagger Controller",
-}).get("/swagger", () => {
+}).get("/v2/swagger", () => {
   return {
     err: "Swagger is disabled",
     message: "Swagger is disabled in production.",
