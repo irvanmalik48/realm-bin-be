58869e4285c150ba60a2c72951c715df2e7f7edf
Irvan Malik <irvanma@gnuweeb.org>
2025-03-26 22:53:24 +0700

    feat(app.routes): reorder
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,16 +2,16 @@ import Elysia from "elysia";
 
 import { RootController } from "./routes/static/infra/static.controller";
 import { GetController } from "./routes/get/infra/get.controller";
-import { PostController } from "./routes/post/infra/post.controller";
-import { config } from "../config";
-import { PutController } from "./routes/post/infra/put.controller";
 import { PostOnboardingController } from "./routes/post/infra/onboarding.controller";
+import { PostController } from "./routes/post/infra/post.controller";
+import { PutController } from "./routes/post/infra/put.controller";
+import { config } from "../config";
 
 const routes = new Elysia({ prefix: config.prefix })
   .use(RootController)
   .use(GetController)
+  .use(PostOnboardingController)
   .use(PostController)
-  .use(PutController)
-  .use(PostOnboardingController);
+  .use(PutController);
 
 export { routes as AppRoutes };
